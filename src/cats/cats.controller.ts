1bcf13f2adf13a9e7632014ca4cd70f097961544
peppapig13132 <peppapig13132@outlook.com>
2024-06-08 01:37:38 +0200

    *

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    return 'This action adds a new cat';
+  }
+
   @Get()
-  findAll(): string {
-    return 'This action returns all cats';
+  async findAll(): Promise<any[]> {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} cat`
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action deletes a #${id} cat`;
   }
 }
