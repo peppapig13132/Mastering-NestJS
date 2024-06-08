a1145f42054e378977327ebef33d4b3bfd7efbd3
peppapig13132 <peppapig13132@outlook.com>
2024-06-08 03:14:27 +0200

    Create a simple provider: cats service

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,17 +1,20 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
+import { CatsService } from './cats.service';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
+
   @Post()
   async create(@Body() createCatDto: CreateCatDto) {
-    return 'This action adds a new cat';
+    this.catsService.create(createCatDto);
   }
 
   @Get()
   async findAll(): Promise<any[]> {
-    return [];
+    return this.catsService.findAll();
   }
 
   @Get(':id')
